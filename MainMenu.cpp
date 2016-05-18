9a6fba38428be2e7c08bb502ab3a3ebb32c81520
scrable <cruss.seq11@gmail.com>
2016-05-17 22:05:37 -0700

    Forgot main menu

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -1,6 +1,6 @@
 #include "MainMenu.h"
 #include "RestaurantDatabase.h"
-//#include "BST.h"
+#include "BST.h"
 #include <cstdlib>
 #include <iostream>
 
@@ -14,18 +14,17 @@ using namespace std;
 
 MainMenu::MainMenu()
 {
+    //create a restaurantDatabase object to call the database functions
+    restaurantDatabase = new RestaurantDatabase();
+
+    //create a pointer to the unique key BST
+    uniqueBSTDatabase = new BST<string>();
 
     //will need to pass in database as an argument to Report() once it is added
     //will also need a new constructor for this to happen
     //this report call to the constructor will create a report object to be used by the main menu object
     //report will be used for search, list, and statistics
-    report = Report();
-
-    //create a restaurant database pointer to the database
-    restaurantDatabase = new RestaurantDatabase();
-
-    //create a pointer to the unique key BST
-   /// uniqueBSTDatabase = new BST<string>();
+    report = Report(uniqueBSTDatabase);
 
    //create a pointer to the secondary key
    /// secondaryBSTDatabase = new BST<string>();
@@ -45,16 +44,17 @@ void MainMenu::showMainMenu()
 	int choice = 0;
 	do
 	{
-        system(CLEAR);
+	    //keep this commented out until we get closer to finishing the project
+        //system(CLEAR);
 
 		//show the mainMenu and get the choice
 		cout << "\t\tRestaurants\n\t\tMain Menu\n\n";
-		cout << "\t    1. Add new data\n";
-		cout << "\t    2. Delete data\n";
-		cout << "\t    3. Search\n";
-		cout << "\t    4. List\n";
-		cout << "\t    5. Statistics\n";
-		cout << "\t    6. Write data to file\n";
+		cout << "\t    1. Add a restaurant to the database\n";
+		cout << "\t    2. Delete a restaurant from the database\n";
+		cout << "\t    3. Search for a restaurant\n";
+		cout << "\t    4. List all restaurants\n";
+		cout << "\t    5. Statistics about the restaurants\n";
+		cout << "\t    6. Write restaurant database to a file\n";
 		cout << "\t    7. Exit\n";
 		cout << "\n\t     Enter your choice: ";
 
@@ -63,6 +63,7 @@ void MainMenu::showMainMenu()
 		if (choice == 1)
 		{
 			//run add new data menu
+			restaurantDatabase->addRestaurant();
 		}
 		else if (choice == 2)
 		{
