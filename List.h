7d2a846b0e7f8a3725d94dc0dee3055b9b5f1521
Corey Russ <cruss.seq11@gmail.com>
2016-06-02 20:12:23 -0700

    Remove unneeded functions

--- List.h
+++ List.h
@@ -36,37 +36,26 @@ public:
     //Default constructor; initializes and empty list
     //Postcondition: all values must be initialized
 
-    List(const List &list);
-    //Copy construcor. Initializes list to have the same elements as another list
-    //Postcondition: a copy of a list is created
-
     ~List();
     //Destructor. Frees memory allocated to the list
     //Postcondition: memory previously allocated is free
 
     /**Accessors*/
 
-
     listitem get_head();
     //Returns the first element in the list
     //Precondition: list must not be empty
-
-
 
     listitem get_tail();
     //Returns the last element in the list
     //Precondition: list must not be empty
 
-
     listitem get_cursor();
     //Returns the element pointed to by the iterator
     //Precondition: list must not be empty
 
-
     bool is_empty();
     //Determines whether a list is empty.
-
-
 
     bool off_end();
     //Determines if the iterator is off the end of the list (i.e. whether cursor is NULL)
@@ -75,12 +64,6 @@ public:
     int get_length();
     //Returns the length of the list
 
-    int getIndex();
-    //Indicates the index of the Node where the cursor is currently pointing
-    //Nodes are numbered from 1 to size of the list
-    //Does not need to be implemented recursively
-    //Pre: !off_end()
-
     int linear_search(listitem value);
     //Searchs the list, element by element, from the head of the List to the tail of the List
     //Returns the index of the value, if it is found in the List
@@ -88,37 +71,12 @@ public:
     //Does not need to be implemented recursively
     //Pre: !empty()
 
-    int binary_search(int low, int high, listitem value);
-    //Recursively searchs the list by dividing the search space in half
-    //Returns the index of the value, if it is found in the List
-    //Returns -1 if the value is not in the List
-    //Pre: !empty()
-    //Pre: List is sorted. Note that you cannot test this with an if statement.
-    //Assumes list is sorted when the function is called.
-
-
     /**Manipulation Procedures*/
 
     void start_cursor();
     //Moves the iterator to point to the first element in the list
     //If the list is empty, the iterator remains NULL
     //Postcondition: cursor either points to NULL or the first element
-
-
-    void insert_cursor(listitem data);
-    //Inserts a new element into the list in the position after the iterator
-    //Precondition: list must not be empty
-    //Postcondition: list contains one new item placed after the cursor
-
-    void delete_tail();
-    //Removes the value of the last element in the list
-    //Precondition: list must not be empty
-    //Postcondition: last element is deleted
-
-    void delete_head();
-    //Removes the value of the first element in the list
-    //Precondition: list must not be empty
-    //Postcondition: first element is deleted
 
     void insert_tail(listitem data);
     //Inserts a new element at the end of the list
@@ -141,63 +99,15 @@ public:
     //Has no effect if the iterator is offend or the list is empty
     //Postcondition: cursor moves to the next element
 
-    void scrollToIndex(int index);
-    //Moves the cursor to the node whose index is specified by the user
-    //Does not need to be implemented recursively
-    //Pre: !empty()
-
-
-    /**Additional List Operations*/
-
     void print();
     //Prints to the console the value of each element in the list sequentially
     //and separated by a blank space
     //Prints nothing if the list is empty
-
-    bool operator==(const List& list);
-    //Overloads the == operator to test of two objects are equal
-    //two lists in this case
-    //returns true of equal
-
-    void printReverse();
-    //Calls the reverse helper function to print a list in reverse
-    //prints nothing if the List is empty
-
-
 };
 
 //constructor
 template <class listitem>
 List<listitem>::List(): length(0), head(NULL), tail(NULL), cursor(NULL) {}
-
-//copy constructor
-template <class listitem>
-List<listitem>::List(const List<listitem> &list): length(list.length)
-{
-    if(list.head == NULL) //If the original list is empty, make an empty list for this list
-    {
-        head = tail = cursor = NULL;
-    }
-    else
-    {
-        head = new Node(list.head->data); //using second Node constructor
-        Nodeptr temp = list.head; //set a temporary node pointer to point at the head of the original list
-        cursor = head; //set iterator to point to head of the new list
-        //cursor->previous = head->previous;
-        //cursor->next = head->next;
-
-        while(temp->next != NULL)
-        {
-            cursor->next = new Node; //using default node constructor to create an empty node
-            cursor->next->previous = cursor;//set the new nodes previous to be the current cursor position
-            cursor = cursor->next; //advance to this new node
-            temp = temp->next; //advance up to the next node in the original list
-            cursor->data = temp->data; //copy the data into the new list
-        }
-        tail = cursor; //Why do I need this line of code?
-        cursor = NULL;
-    }
-}
 
 //destructor
 template <class listitem>
